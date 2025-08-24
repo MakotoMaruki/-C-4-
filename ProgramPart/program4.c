cb0add2b043ffdb3f6dc9836d099fb0860f6c414
Makoto Maruki <cccp.perseus@gmail.com>
2025-08-24 12:55:05 +0800

    Create program4.c

---
+++ ProgramPart/program4.c
@@ -0,0 +1,17 @@
+//
+//  program4.c
+//  
+//
+//  Created by Makoto Maruki on 2025/8/24.
+//
+
+
+#include <stdio.h>
+#define G 10
+int main(){
+    float h;
+    int t=3;
+    h=(G*t*t)/2;
+    printf("height = %.2f",h);
+    return 0;
+}
