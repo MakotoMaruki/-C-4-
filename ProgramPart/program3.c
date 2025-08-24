fb8dbcceafc096385cefa274c9c7993ae266608e
Makoto Maruki <cccp.perseus@gmail.com>
2025-08-24 12:48:30 +0800

    Create program3.c

---
+++ ProgramPart/program3.c
@@ -0,0 +1,15 @@
+//
+//  program3.c
+//  
+//
+//  Created by Makoto Maruki on 2025/8/24.
+//
+
+#include <stdio.h>
+int main(void){
+    int C;
+    int F=150;
+    C=5*(F-32)/9;
+    printf("fahr = 150, celsius = %d",C);
+    return 0;
+}
