8c33723ba2d60924f561bd26a7a1d70895bc3540
Makoto Maruki <cccp.perseus@gmail.com>
2025-08-24 12:48:33 +0800

    Create program2.c

---
+++ ProgramPart/program2.c
@@ -0,0 +1,16 @@
+//
+//  program2.c
+//  
+//
+//  Created by Makoto Maruki on 2025/8/24.
+//
+
+#include <stdio.h>
+int main(){
+    printf("* * * *\n");
+    printf(" * * *\n");
+    printf("  * *\n");
+    printf("   *");
+    return 0;
+    
+}
