fb30061e1f33ed6dda2523ca9c749162aebfcbc7
Makoto Maruki <cccp.perseus@gmail.com>
2025-08-24 12:55:07 +0800

    Create program5.c

---
+++ ProgramPart/program5.c
@@ -0,0 +1,17 @@
+//
+//  program5.c
+//  
+//
+//  Created by Makoto Maruki on 2025/8/24.
+//
+
+
+#include <stdio.h>
+int main(void){
+    int C;
+    int F;
+    scanf("%d",&F);
+    C=5*(F-32)/9;
+    printf("Celsius = %d",C);
+    return 0;
+}
